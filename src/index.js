30e3d3bc135ae0a72a1d11d354d85963dc50ddb9
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-11-02 18:34:09 +0300

    Prettier updated

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React from 'react';
+import styles from './styles.module.css';
 
-export const Paper = ({ children, elevation, outlined, square, customClass }) => {
-  const elevationStyles = elevation ? `elevation-${elevation}` : ''
-  const outlinedStyles = outlined ? 'outlined' : ''
-  const basicStyles = square ? 'square' : 'round'
-  const customStyles = customClass ? `${customClass}` : ''
+export const Paper = ({
+  children,
+  elevation,
+  outlined,
+  square,
+  customClass
+}) => {
+  const elevationStyles = elevation ? `elevation-${elevation}` : '';
+  const outlinedStyles = outlined ? 'outlined' : '';
+  const basicStyles = square ? 'square' : 'round';
+  const customStyles = customClass ? `${customClass}` : '';
   return (
     <div
       className={`${styles[basicStyles]} ${styles[elevationStyles]} ${styles[outlinedStyles]} ${customStyles}`}
     >
       {children}
     </div>
-  )
-}
+  );
+};
