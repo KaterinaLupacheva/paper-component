d0484ff6d7f2ddd81a563ae0677538b96d654f2f
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-11-02 18:22:27 +0300

    Square prop

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import './App.css';
+import './App.css'
 import { Paper } from '@ramonak/paper'
 import '@ramonak/paper/dist/index.css'
 
 const App = () => {
-  return <Paper elevation={5} outlined customStyles="paper-class" />
+  return <Paper elevation={5} outlined customClass='paper-class' square />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const Paper = ({ children, elevation, outlined, customClass }) => {
+export const Paper = ({ children, elevation, outlined, square, customClass }) => {
   const elevationStyles = elevation ? `elevation-${elevation}` : ''
   const outlinedStyles = outlined ? 'outlined' : ''
-  const customStyles = customStyles ? `${customStyles}` : ''
+  const basicStyles = square ? 'square' : 'round'
+  const customStyles = customClass ? `${customClass}` : ''
   return (
     <div
-      className={`${styles['paper']} ${styles[elevationStyles]} ${styles[outlinedStyles]} ${customStyles}`}
+      className={`${styles[basicStyles]} ${styles[elevationStyles]} ${styles[outlinedStyles]} ${customStyles}`}
     >
       {children}
     </div>
