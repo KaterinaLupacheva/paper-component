d97dcb9b4588c21423c5c84456ea01ee0900e012
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-11-02 18:15:58 +0300

    Custom styles added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const Paper = ({ children, elevation, outlined }) => {
-  const elevationStyles = elevation ? `elevation-${elevation}` : '';
-  const outlinedStyles = outlined ? 'outlined' : '';
-  return <div className={`${styles.paper} ${styles[elevationStyles]} ${styles[outlinedStyles]}`}>{children}</div>
+export const Paper = ({ children, elevation, outlined, customClass }) => {
+  const elevationStyles = elevation ? `elevation-${elevation}` : ''
+  const outlinedStyles = outlined ? 'outlined' : ''
+  const customStyles = customStyles ? `${customStyles}` : ''
+  return (
+    <div
+      className={`${styles['paper']} ${styles[elevationStyles]} ${styles[outlinedStyles]} ${customStyles}`}
+    >
+      {children}
+    </div>
+  )
 }
