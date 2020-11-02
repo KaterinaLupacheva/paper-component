99d469a4a42b6e2a6f6be8c295de5b844c834ee6
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-11-02 16:53:16 +0300

    Outlined variant added

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { Paper } from '@ramonak/paper'
 import '@ramonak/paper/dist/index.css'
 
 const App = () => {
-  return <Paper elevation={20}/>
+  return <Paper elevation={5} outlined />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const Paper = ({ children, elevation }) => {
-  const elStyle = elevation ? `elevation-${elevation}` : ''
-  return <div className={`${styles.paper} ${styles[elStyle]}`}>{children}</div>
+export const Paper = ({ children, elevation, outlined }) => {
+  const elevationStyles = elevation ? `elevation-${elevation}` : '';
+  const outlinedStyles = outlined ? 'outlined' : '';
+  return <div className={`${styles.paper} ${styles[elevationStyles]} ${styles[outlinedStyles]}`}>{children}</div>
 }
