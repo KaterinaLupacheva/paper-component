1483bc3dca1eb74fd230619d6ff86e9a8251d6e2
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-11-03 16:55:25 +0300

    Updates

---
+++ precache-manifest.abda09e58d91d0dacd4e27b4298ce1d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c0057d26f3b2ac1cb1e93348266a0b3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ab79832e3b4646b8d52f",
+    "url": "./static/css/main.d6e75a2a.chunk.css"
+  },
+  {
+    "revision": "cc43e0f1f569dcb75b13",
+    "url": "./static/js/2.24bc0b4c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.24bc0b4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab79832e3b4646b8d52f",
+    "url": "./static/js/main.04aaf4f0.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.04aaf4f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe447ee4c66f8d51978",
+    "url": "./static/js/runtime-main.e3fc9297.js"
+  }
+]);
