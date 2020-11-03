6080c971458a641202288ab5b00fa57fd1992954
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-11-03 13:53:00 +0300

    Demo app updated

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,26 @@ import { Paper } from '@ramonak/paper';
 import '@ramonak/paper/dist/index.css';
 
 const App = () => {
-  return <Paper elevation={7} outlined customClass='paper-class' square />;
+  return (
+    <div className='wrapper'>
+      <Paper customClass='paper-class'>
+        <div className='text'>
+          <b>Default:</b> <br />
+          No Elevation <br /> No Outline <br /> Rounded corners{' '}
+        </div>
+      </Paper>
+      <Paper elevation={3} customClass='paper-class'>
+        <div className='text'>
+          Elevation = 3 <br /> No Outline <br /> Rounded corners{' '}
+        </div>
+      </Paper>
+      <Paper elevation={7} outlined customClass='paper-class' square>
+        <div className='text'>
+          Elevation = 7 <br /> Oulined <br /> Square{' '}
+        </div>
+      </Paper>
+    </div>
+  );
 };
 
 export default App;
